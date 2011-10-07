04fca2740a20b324f93f18504bb91e75f15fecd2
Thomas Schank <DrTom@schank.ch>
2011-10-07 14:40:58 +0200

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadekPlayground::Application.config.secret_token = '893131b7bb92a93f6b8ca24fdb27ec943b77f098302bf53e88e85c0eecacbbdfcddda81f3a2f165f78207075a5289ca604e00c21a90ad8e9ce5cae852dbe118b'
